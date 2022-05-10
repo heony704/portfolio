1d97ddbf36f068b27af55329aab3f3725d051206
SeungHe0n <014sh@naver.com>
2022-05-10 20:11:43 +0900

    profile 에 english 추가

--- src/data.js
+++ src/data.js
@@ -1,14 +1,14 @@
 const data = {
     profile: {
         self: '/images/self.png',
-        name: '이승헌  |  Seungheon Lee',
+        name: '이승헌',
+        english: 'Seungheon Lee',
         job: 'Front-End Programmer',
         email: 'lsheony704@gmail.com',
         github: 'github.com/SeungHe0n',
         velog: 'velog.io/@heony'
     },
     about: {
-        // title: '사용자처럼 생각하는 개발자',
         contents:
             '안녕하세요! 사용자처럼 생각하는 프론트엔드 개발자, 이승헌입니다. \nJavaScript, TypeScript, React를 배우고 있으며 백엔드에도 관심이 있습니다. 작은 차이로 사용자 경험이 더 좋아지는 순간을 좋아합니다.'
     },
