2bd4ad897fcefa18d76527db85f5549c9cc259c7
SeungHe0n <014sh@naver.com>
2022-05-11 21:53:27 +0900

    pdf 버튼 누르면 pdf 저장

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,20 @@
 import '../styles/header.scss';
 import { BsDownload } from 'react-icons/bs';
-// import ReactPDF from '@react-pdf/renderer';
-import { Document, Page, Text } from '@react-pdf/renderer';
-// import PortfolioPdf from './PortfolioPdf';
+import { PDFDownloadLink } from '@react-pdf/renderer';
+import PortfolioPdf from './PortfolioPdf';
 
 function Header({ portfolio }) {
-    const onClick = () => {
-        // ReactPDF.render(<PortfolioPdf />, `example.pdf`);
-    };
-
     return (
         <header>
             <div>
                 <div className="header">
                     <p className="logo">HEONY's POFO</p>
-                    <div className="downloadBtn" onClick={onClick}>
+                    <PDFDownloadLink
+                        document={<PortfolioPdf portfolio={portfolio} />}
+                        fileName={`${portfolio.profile.name} 포트폴리오.pdf`}
+                    >
                         <BsDownload />
-                    </div>
+                    </PDFDownloadLink>
                 </div>
             </div>
         </header>
--- src/components/PortfolioPdf.js
+++ src/components/PortfolioPdf.js
@@ -1,15 +1,68 @@
 import React from 'react';
-import { Document, Page, Text } from '@react-pdf/renderer';
-import data from '../data';
+import {
+    StyleSheet,
+    Font,
+    Document,
+    Page,
+    View,
+    Image,
+    Text
+} from '@react-pdf/renderer';
 
-const styles = StyleSheet.create({});
-
-const PortfolioPdf = () => (
+const PortfolioPdf = ({ portfolio }) => (
     <Document>
-        <Page>
-            <Text>TEST HING</Text>
-        </Page>
+        <Page size={'A4'} style={styles.page}></Page>
     </Document>
 );
 
 export default PortfolioPdf;
+
+const styles = StyleSheet.create({
+    page: {
+        paddingHorizontal: 40,
+        paddingVertical: 60
+    },
+    title: { fontFamily: 'Pretendard' }
+});
+
+Font.register({
+    family: 'Pretendard',
+    fonts: [
+        {
+            src: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/public/static/alternative/Pretendard-Thin.ttf',
+            fontWeight: 100
+        },
+        {
+            src: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/public/static/alternative/Pretendard-Light.ttf',
+            fontWeight: 200
+        },
+        {
+            src: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/public/static/alternative/Pretendard-ExtraLight.ttf',
+            fontWeight: 300
+        },
+        {
+            src: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/public/static/alternative/Pretendard-Regular.ttf',
+            fontWeight: 400
+        },
+        {
+            src: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/public/static/alternative/Pretendard-Medium.ttf',
+            fontWeight: 500
+        },
+        {
+            src: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/public/static/alternative/Pretendard-SemiBold.ttf',
+            fontWeight: 600
+        },
+        {
+            src: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/public/static/alternative/Pretendard-Bold.ttf',
+            fontWeight: 700
+        },
+        {
+            src: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/public/static/alternative/Pretendard-ExtraBold.ttf',
+            fontWeight: 800
+        },
+        {
+            src: 'https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/public/static/alternative/Pretendard-Black.ttf',
+            fontWeight: 900
+        }
+    ]
+});
